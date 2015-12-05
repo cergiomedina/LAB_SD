e6d90771711e55f62379604d2ea387f1cdbae121
sergio <sergio@slinux>
2015-12-05 20:18:46 -0300

    algunos cambios simples

--- labSD/src/laboratorio_1_sd/FrontService.java
+++ labSD/src/laboratorio_1_sd/FrontService.java
@@ -1,9 +1,6 @@
 package laboratorio_1_sd;
 
-/**
- *
- * @author giovanni
- */
+
 public class FrontService {
 
     static String[] queries = {"query 1", "query 2", "query 3", "query 4", "query 5", "query 6", "query 7", "query 8", "query 9", "query 10", "query 11", "query 12", "query 13", "query 14", "query 15", "query 16", "query 17", "query 18", "query 19", "query 20"};
