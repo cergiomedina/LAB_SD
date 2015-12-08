c4290b78226fb439a52f7fea44eae536a0395e73
seerg <seerg@DESKTOP-IMRT9DB>
2015-12-07 23:10:19 -0300

    solo busqueda distribuida en cache, sin hilos aun

--- labSD/src/laboratorio_1_sd/LRUCache.java
+++ labSD/src/laboratorio_1_sd/LRUCache.java
@@ -5,21 +5,86 @@ import java.util.LinkedHashMap;
 import java.net.*;
 import java.io.*;
 
-public class LRUCache {
+public class LRUCache extends Thread {
 
     static LinkedHashMap<String, String> cache;
-    static int size = 5;
+    static int size = 20;
+    static LinkedHashMap<String,String> cache1;
+    static LinkedHashMap<String,String> cache2;
+    static LinkedHashMap<String,String> cache3;
+    static LinkedHashMap<String,String> cache4;
+    static LinkedHashMap<String,String> cache5;
     
     public static void main(String[] args) {
        
         initServer();
+      
+    }
+
+    private static String busquedaEnCache(String mensajeRecibido) {
+        String resultado = null;
+        
+       switch(mensajeRecibido.charAt(0)){
+                case 'a':
+                case 'b':
+                case 'c':
+                case 'd':
+                        resultado = getEntryFromCache(cache,mensajeRecibido);
+                    break;
+                case 'e':
+                case 'f':
+                case 'g':
+                case 'h':
+                        resultado = getEntryFromCache(cache1,mensajeRecibido);
+                    break;
+                case 'i':
+                case 'j':
+                case 'k':
+                case 'l':
+                        resultado = getEntryFromCache(cache2,mensajeRecibido);
+                    break;
+                case 'm':
+                case 'n':
+                case 'o':
+                case 'p':
+                        resultado = getEntryFromCache(cache3,mensajeRecibido);
+                    break;
+                case 'q':
+                case 'r':
+                case 's':
+                case 't':
+                        resultado = getEntryFromCache(cache4,mensajeRecibido);
+                    break;
+                case 'u':
+                case 'v':
+                case 'w':
+                case 'x':
+                case 'y':
+                case 'z':
+                        resultado = getEntryFromCache(cache5,mensajeRecibido);
+                    break;
+                    
+            }
+       return resultado;
+    }
+
+    public void run(){
+        
+            
+        
+        
     }
     
-    public static void initServer(){
+    public static void initServer (){
+        cache = new LinkedHashMap<>();
+        cache1 = new LinkedHashMap<>();
+        cache2 = new LinkedHashMap<>();
+        cache3 = new LinkedHashMap<>();
+        cache4 = new LinkedHashMap<>();
+        cache5 = new LinkedHashMap<>();
         final int PUERTO=5000;
         ServerSocket sc;
         Socket so;
-        String MensajeRecibido;
         DataOutputStream salida;
         BufferedReader entrada;
         String mensajeRecibido;
@@ -33,6 +98,13 @@ public class LRUCache {
             mensajeRecibido = entrada.readLine();
             salida.writeUTF(mensajeRecibido);
             System.out.println("CACHE RECIBIO COMO PREGUNTA: "+mensajeRecibido);
+            String resultado = null;
+            
+            resultado = busquedaEnCache(mensajeRecibido);
+
+            System.out.println("EL RESULTADO DE LA BUSQUEDA ES: "+resultado);
+            
+
             sc.close();
             
         } catch (Exception e) {
@@ -42,7 +114,7 @@ public class LRUCache {
         
     }
 
-    public String getEntryFromCache(String query) {
+    public static String getEntryFromCache(LinkedHashMap<String,String> cache,String query) {
         String result = cache.get(query);
         if(result != null) {
             cache.remove(query);
